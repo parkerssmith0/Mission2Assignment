8e1820e9722b213353351dae5a6fc41bde8bbddf
parkerssmith0 <parkerssmith0@gmail.com>
2023-01-19 08:17:02 -0700

    comments

--- Mission2Assignment/Program.cs
+++ Mission2Assignment/Program.cs
@@ -6,28 +6,35 @@ using System.Threading.Tasks;
 
 namespace Mission2Assignment
 {
+    //declaring the class
     class Program
     {
         static void Main(string[] args)
         {
-
+            //asking the user to input the # of dice rolls
             Console.Write("Welcome to the dice throwing simulator!\n");
             Console.Write("How many dice rolls would you like to simulate? ");
             string val = Console.ReadLine();
             int rolls = Convert.ToInt32(val);
 
+            //creating the list of 11 different roll options
             float[] listRolls = new float[11];
 
+            //Creating the random roll
             Random dice = new Random();
 
+            //Looping through the # of rolls and adding the results to the roll option list
             for (int i = 0; i < rolls; i++)
             {
                 int rollTotal = (dice.Next(11) + 1);
                 listRolls[rollTotal - 1]++;
             }
+
             Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
             Console.WriteLine("Each '*' represents 1 % of the total number of rolls.");
             Console.WriteLine("Total number of rolls = " + rolls + ".\n");
+
+            //Calculating the # of rolls and creating the 1 % of rolls per number chosen
             for (int x = 0; x < listRolls.Length; x++)
             {
                 float fRollCount = ((listRolls[x]/rolls)*100);
